15a5602950d981f12143308d470c2cb117b3407b
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-17 09:57:21 -0800

    connect with frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 
 
 import authRoute from "./routes/auth.routes.js"
@@ -8,8 +9,18 @@ import messageRoute from "./routes/message.routes.js"
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 app.use("/api/auth",authRoute)
 app.use("/api/message",messageRoute)
+
+
+if(process.env.NODE_ENv = "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.use("*",(req,res)=>{
+        res.sendFile(__dirname, "/frontend","index.html")
+    })
+}
 
  app.listen(PORT,()=> console.log(`Server listen on ${PORT}`)) 
