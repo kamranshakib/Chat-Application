42322a3561f29608301293d79d2f370e8e7a0e73
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-19 10:38:54 -0800

    Welcome email implemened

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
     unique: true,
     
   },
-  password: {
+  password: { 
     type: String,
     required: true,
     minlength: 6,
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 const Router = express.Router();
 import { login, logout, signup } from "../controller/auth.controller.js";
+import User from "../models/User.js";
 
 Router.post("/signup", signup);
 
@@ -8,4 +9,22 @@ Router.get("/login", login);
 
 Router.get("/logout", logout);
 
+// for testing sender email
+Router.get("/delete-email", async (req,res)=>{
+    try {
+        const result =  await User.deleteMany({}) 
+        res.status(200).json({
+      success: true,
+      message: "All emails deleted successfully",
+      deletedCount: result.deletedCount,
+         });
+        
+    } catch (error) {
+        res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+        
+    }
+})
 export default Router;
