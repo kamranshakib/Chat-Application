35936181a7046a3d52f3304dc77d043de426f9cb
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-24 09:02:27 -0800

    get all chat messages between me and user  or user and me

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -13,11 +13,31 @@ export const getAllContacts = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
 export const getAllChats = (req, res) => {
   res.send("get all chats");
 };
-export const getMessageByUserId = (req, res) => {
-  res.send("get message by user id");
+
+
+export const getMessageByUserId =async (req, res) => {
+    try {
+        const MyId = req.user._id;
+        const {id: userToChatId} = req.params;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:MyId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:MyId}
+            ]
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }
+ 
 };
 export const sendMessage = (req, res) => {
   res.send("send message");
