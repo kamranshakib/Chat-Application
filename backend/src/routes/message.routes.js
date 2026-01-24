5d07519f1d59b7591c7cc225cb36883593aba7c5
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-24 10:00:51 -0800

    get all sender or receiver accounts chat

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -2,15 +2,16 @@ import express from "express";
 import {
   getAllContacts,
   getAllChats,
-  getMessageByUserId,
+  getMessageByUserId, 
   sendMessage,
 } from "../controller/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const Router = express.Router();
-
-Router.get("/contacts",protectRoute, getAllContacts);
-Router.get("/chats",protectRoute, getAllChats);
-Router.get("/:id",protectRoute, getMessageByUserId);
-Router.post("/send/:id",protectRoute, sendMessage);
-
+  
+Router.use(arcjetProtection, protectRoute);
+Router.get("/contacts", getAllContacts);
+Router.get("/chats", getAllChats);
+Router.get("/:id", getMessageByUserId);
+Router.post("/send/:id", sendMessage);
 export default Router;
