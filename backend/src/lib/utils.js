1a743a5fce5047fd3acbcb9e099318781e9fa8b9
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-27 08:48:52 -0800

    refactoring frontend code

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,17 +2,15 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (id, res) => {
-  const {JWT_SECRET} = ENV;
-  if(!JWT_SECRET) throw new Error("JWT_SECRET is not configured");
-  const token = jwt.sign({ id }, JWT_SECRET,
-    { expiresIn: "7d" });
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) throw new Error("JWT_SECRET is not configured");
+  const token = jwt.sign({ id }, JWT_SECRET, { expiresIn: "7d" });
 
-   res.cookie("jwt",token,{
-    maxAge:  7 * 24 * 60 * 60  * 1000, 
-    httpOnly: true, // prevent XSS attack 
-    sameSite: "strict" // prevent CSRF attacks
-   })  
-   
-   return token
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // prevent XSS attack
+    sameSite: "strict", // prevent CSRF attacks
+  });
 
+  return token;
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ const PORT = ENV.PORT || 5000;
 // middleware
 app.use(cookieParser());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
