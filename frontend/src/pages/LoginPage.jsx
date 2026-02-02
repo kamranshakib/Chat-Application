de73e9d2b536301f34a489735d85fbaf9dcbd9d6
khaledsaifee <saifeekhaled1212@gmail.com>
2026-02-02 05:14:11 -0800

    Set text direction to right-to-left for LoginPage
    Added 'dir="rtl"' attribute to the main container.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ function LoginPage() {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]" dir="rtl">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row-reverse">
             {/* FORM CLOUMN - LEFT SIDE */}
